6c611bd3ff4c7c68e9ec029c9775f9212232d631
Avadhut Jagde <avadhutjagde555@gmail.com>
2025-01-26 17:10:49 +0530

    [Course: The Ultimate TypeScript Course > 10-React with TypeScript (29m) ] [ Video: #4-Creating-a-Component_mp4_5m_11s ] -

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/reminder";
+
+const reminders: Reminder[] = [{ id: 1, title: "Reminder 1" }];
 
 function App() {
   return (
     <>
       <div className="App">
-        <button className="btn btn-primary">Click Me</button>
+        <ReminderList items={reminders} />
       </div>
     </>
   );
